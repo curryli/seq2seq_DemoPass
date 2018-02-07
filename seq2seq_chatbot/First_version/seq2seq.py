2a4d2e80803afef9f131137b9150b7ef94af04e5
lixurui <xurui.lee@msn.com>
2018-02-07 17:25:50 +0800

    修改了测试阶段Linear 找不到这个函数，从r1.2版本rnn_cell_impl复制过来

--- seq2seq_chatbot/First_version/seq2seq.py
+++ seq2seq_chatbot/First_version/seq2seq.py
@@ -21,6 +21,73 @@ from tensorflow.python.ops import rnn
 from tensorflow.python.ops import rnn_cell_impl
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.util import nest
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops import init_ops
+
+_BIAS_VARIABLE_NAME = "bias"
+_WEIGHTS_VARIABLE_NAME = "kernel"
+
+def Linear(args,
+            output_size,
+            bias,
+            bias_initializer=None,
+            kernel_initializer=None):
+  """Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
+  Args:
+    args: a 2D Tensor or a list of 2D, batch x n, Tensors.
+    output_size: int, second dimension of W[i].
+    bias: boolean, whether to add a bias term or not.
+    bias_initializer: starting value to initialize the bias
+      (default is all zeros).
+    kernel_initializer: starting value to initialize the weight.
+  Returns:
+    A 2D Tensor with shape [batch x output_size] equal to
+    sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+  Raises:
+    ValueError: if some of the arguments has unspecified or wrong shape.
+  """
+  if args is None or (nest.is_sequence(args) and not args):
+    raise ValueError("`args` must be specified")
+  if not nest.is_sequence(args):
+    args = [args]
+
+  # Calculate the total size of arguments on dimension 1.
+  total_arg_size = 0
+  shapes = [a.get_shape() for a in args]
+  for shape in shapes:
+    if shape.ndims != 2:
+      raise ValueError("linear is expecting 2D arguments: %s" % shapes)
+    if shape[1].value is None:
+      raise ValueError("linear expects shape[1] to be provided for shape %s, "
+                       "but saw %s" % (shape, shape[1]))
+    else:
+      total_arg_size += shape[1].value
+
+  dtype = [a.dtype for a in args][0]
+
+  # Now the computation.
+  scope = vs.get_variable_scope()
+  with vs.variable_scope(scope) as outer_scope:
+    weights = vs.get_variable(
+        _WEIGHTS_VARIABLE_NAME, [total_arg_size, output_size],
+        dtype=dtype,
+        initializer=kernel_initializer)
+    if len(args) == 1:
+      res = math_ops.matmul(args[0], weights)
+    else:
+      res = math_ops.matmul(array_ops.concat(args, 1), weights)
+    if not bias:
+      return res
+    with vs.variable_scope(outer_scope) as inner_scope:
+      inner_scope.set_partitioner(None)
+      if bias_initializer is None:
+        bias_initializer = init_ops.constant_initializer(0.0, dtype=dtype)
+      biases = vs.get_variable(
+          _BIAS_VARIABLE_NAME, [output_size],
+          dtype=dtype,
+          initializer=bias_initializer)
+    return nn_ops.bias_add(res, biases)
+
 
 def _extract_beam_search(embedding, beam_size, num_symbols, embedding_size, output_projection=None):
 
@@ -128,3 +195,129 @@ def embedding_attention_seq2seq(encoder_inputs,
             feed_previous=feed_previous,
             initial_state_attention=initial_state_attention, beam_search=beam_search, beam_size=beam_size)
 
+
+def beam_attention_decoder(decoder_inputs,
+                          initial_state,
+                          attention_states,
+                          cell,
+                           embedding,
+                          output_size=None,
+                          num_heads=1,
+                          loop_function=None,
+                          dtype=None,
+                          scope=None,
+                          initial_state_attention=False, output_projection=None, beam_size=10):
+    if not decoder_inputs:
+        raise ValueError("Must provide at least 1 input to attention decoder.")
+    if num_heads < 1:
+        raise ValueError("With less than 1 heads, use a non-attention decoder.")
+    if not attention_states.get_shape()[1:2].is_fully_defined():
+        raise ValueError("Shape[1] and [2] of attention_states must be known: %s"
+                         % attention_states.get_shape())
+    if output_size is None:
+        output_size = cell.output_size
+
+    with variable_scope.variable_scope(scope or "attention_decoder", dtype=dtype) as scope:
+        dtype = scope.dtype
+        # batch_size = array_ops.shape(decoder_inputs[0])[0]  # Needed for reshaping.
+        attn_length = attention_states.get_shape()[1].value
+        if attn_length is None:
+            attn_length = array_ops.shape(attention_states)[1]
+        attn_size = attention_states.get_shape()[2].value
+
+        # To calculate W1 * h_t we use a 1-by-1 convolution, need to reshape before.
+        hidden = array_ops.reshape(attention_states, [-1, attn_length, 1, attn_size])
+        hidden_features = []
+        v = []
+        attention_vec_size = attn_size  # Size of query vectors for attention.
+        for a in xrange(num_heads):
+            k = variable_scope.get_variable("AttnW_%d" % a, [1, 1, attn_size, attention_vec_size])
+            hidden_features.append(nn_ops.conv2d(hidden, k, [1, 1, 1, 1], "SAME"))
+            v.append(variable_scope.get_variable("AttnV_%d" % a, [attention_vec_size]))
+
+        state = []
+        # 将encoder的最后一个隐层状态扩展成beam_size维，因为decoder阶段的batch_size是beam_size。
+        # initial_state是一个列表，RNN有多少层就有多少个元素，每个元素都是一个LSTMStateTuple，包含h,c两个隐层状态
+        # 所以要将其扩展成beam_size维，其实是把c和h进行扩展，最后再合成LSTMStateTuple就可以了
+        for layers in initial_state:
+            c = [layers.c] * beam_size
+            h = [layers.h] * beam_size
+            c = tf.concat(c, 0)
+            h = tf.concat(h, 0)
+            state.append(rnn_cell_impl.LSTMStateTuple(c, h))
+        state = tuple(state)
+        # state_size = int(initial_state.get_shape().with_rank(2)[1])
+        # states = []
+        # for kk in range(beam_size):
+        #     states.append(initial_state)
+        # state = tf.concat(states, 0)
+        # state = initial_state
+
+        def attention(query):
+            ds = []  # Results of attention reads will be stored here.
+            if nest.is_sequence(query):  # If the query is a tuple, flatten it.
+                query_list = nest.flatten(query)
+                for q in query_list:  # Check that ndims == 2 if specified.
+                    ndims = q.get_shape().ndims
+                    if ndims:
+                        assert ndims == 2
+                query = array_ops.concat(query_list, 1)
+            for a in xrange(num_heads):
+                with variable_scope.variable_scope("Attention_%d" % a):
+                    y = Linear(query, attention_vec_size, True)#(query)
+                    y = array_ops.reshape(y, [-1, 1, 1, attention_vec_size])
+                    # Attention mask is a softmax of v^T * tanh(...).
+                    s = math_ops.reduce_sum(v[a] * math_ops.tanh(hidden_features[a] + y), [2, 3])
+                    a = nn_ops.softmax(s)
+                    # Now calculate the attention-weighted vector d.
+                    d = math_ops.reduce_sum(array_ops.reshape(a, [-1, attn_length, 1, 1]) * hidden, [1, 2])
+                    ds.append(array_ops.reshape(d, [-1, attn_size]))
+            return ds
+
+        outputs = []
+        prev = None
+        # attention也要定义成beam_size为的tensor
+        batch_attn_size = array_ops.stack([beam_size, attn_size])
+        attns = [array_ops.zeros(batch_attn_size, dtype=dtype) for _ in xrange(num_heads)]
+        for a in attns:  # Ensure the second shape of attention vectors is set.
+            a.set_shape([None, attn_size])
+        if initial_state_attention:
+            attns = attention(initial_state)
+
+        log_beam_probs, beam_path, beam_symbols = [], [], []
+        for i, inp in enumerate(decoder_inputs):
+            if i > 0:
+                variable_scope.get_variable_scope().reuse_variables()
+            # If loop_function is set, we use it instead of decoder_inputs.
+            if i == 0:
+                #i=0时，输入时一个batch_szie=beam_size的tensor，且里面每个元素的值都是相同的，都是<GO>标志
+                inp = tf.nn.embedding_lookup(embedding, tf.constant(1, dtype=tf.int32, shape=[beam_size]))
+
+            if loop_function is not None and prev is not None:
+                with variable_scope.variable_scope("loop_function", reuse=True):
+                    inp = loop_function(prev, i, log_beam_probs, beam_path, beam_symbols)
+            # Merge input and previous attentions into one vector of the right size.
+            input_size = inp.get_shape().with_rank(2)[1]
+            if input_size.value is None:
+                raise ValueError("Could not infer input size from input: %s" % inp.name)
+            inputs = [inp] + attns
+            x = Linear(inputs, input_size, True)#(inputs)
+
+            # Run the RNN.
+            cell_output, state = cell(x, state)
+            # Run the attention mechanism.
+            if i == 0 and initial_state_attention:
+                with variable_scope.variable_scope(variable_scope.get_variable_scope(), reuse=True):
+                    attns = attention(state)
+            else:
+                attns = attention(state)
+
+            with variable_scope.variable_scope("AttnOutputProjection"):
+                inputs = [cell_output] + attns
+                output = Linear(inputs, output_size, True)#(inputs)
+            if loop_function is not None:
+                prev = output
+            outputs.append(tf.argmax(nn_ops.xw_plus_b(output, output_projection[0], output_projection[1]), axis=1))
+
+    return outputs, state, tf.reshape(tf.concat(beam_path, 0), [-1, beam_size]), tf.reshape(tf.concat(beam_symbols, 0),
+                                                                                            [-1, beam_size])
